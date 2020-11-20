b33e67e148cbda017372d4b8c96d486daa82d22b
Benny Serra <benjaserrau@gmail.com>
2020-11-20 00:55:28 -0300

    prediction function changed with vad values

--- RESTapi/views.py
+++ RESTapi/views.py
@@ -10,30 +10,62 @@ from rest_framework.permissions import AllowAny
 from django.views.generic.base import TemplateView
 
 # category labels
-labels = ['confident', 'unconfident', 'handles pressure', 'cant handle pressure', 'interested',
-          'uninsterested', 'happy', 'unhappy', 'friendly', 'unfriendly']
+main_labels = ['confident', 'unconfident',
+               'pos_hp', 'neg_hp',
+               'interested', 'uninterested',
+               'happy', 'unhappy',
+               'friendly', 'unfriendly'
+               ]
 
-label_dict = dict(zip(labels, range(1, len(labels) + 1)))
+# to dictionary
+label_dict = dict(zip(main_labels, range(0, len(main_labels))))
+
+# inverting label_dict
+inv_label = {v: k for k, v in label_dict.items()}
 
 # Load the pretrained model
-model_path = 'C:/Users/Acer/Documents/coding/Upwork/Peter Gretale/models/h5/words/model_1000.h5'
-model = load_model(model_path)
+model = load_model('tfmodel.h5')
 
 
-def preprocess(x, padding_shape=30):
-    return np.array([ord(i.lower()) - ord('a')+1 if not i.isdigit() and i != ' ' else 0 for i in list(x)] + ([0] * (padding_shape - len(x))), dtype=int)
+def get_embedding_vec(word):
+    """
+    return : embedding vector of a word
+    """
+    idx = word_embedding.dictionary.get(word.lower(), -1)
+
+    if idx < 0:
+        return np.zeros((EMBEDDING_DIM, ), dtype='float32')  # UNK
+
+    return word_embedding.word_vectors[idx]
 
 
-def predict(d: dict, s: str, model):
-    token = preprocess(s)
-    output = model.predict(np.array([token]))
-    ID = int(tf.keras.backend.argmax(output))
+def predict_class(word, valence, arousal, dominance, model):
+    """
+    a function that predicts sentiments from words
 
-    for k, v in d.items():
-        if v == ID:
-            return k
+    ARGUMENTS : 
 
-    return 'unclassified'
+    word : our word (example: happy, tolerant ...)
+
+    valence,arousal,dominance,quadrant : important features that boost the accuracy of the model
+
+    return : class predicted
+
+    """
+
+    # creating the embedding of the word
+    embedding_features = get_embedding_vec(word)
+
+    feats_without_embedding = [valence, arousal, dominance]
+
+    # creating our X that contains all features (word_embeddings, VADQ)
+    x_t = np.concatenate((feats_without_embedding, embedding_features)).reshape(
+        1, 53)  # 54 if we are using quadrant as parameter
+
+    # predictions (we will take saved models of each fold and do an average of predictions)
+    prediction = model.predict(x=x_t, verbose=1)
+
+    return inv_label[np.argmax(prediction)]
 
 
 # Classification call class
@@ -43,11 +75,14 @@ class Classification(APIView):
 
     def post(self, request):
         try:
-            # text to predict
-            text = request.POST.get('word')
+            # word to predict with VAD values
+            word = request.POST.get('word')
+            valance = request.POST.get('valance')
+            arousal = request.POST.get('arousal')
+            dominance = request.POST.get('dominance')
 
             # calling prediction function
-            pred = predict(label_dict, text, model)
+            pred = predict_class(word, valance, arousal, dominance, model)
             print('Prediction: {}'.format(pred))
 
             return JsonResponse({'message': 'Prediction: ' + str(pred), 'code': 200, 'status': 'Success'})
